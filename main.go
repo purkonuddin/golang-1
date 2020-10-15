071808854e881a514137dd4a2ca94be978a255a5
purkonuddin <44079569+purkonuddin@users.noreply.github.com>
2020-10-15 22:50:35 +0700

    interface
    segi tiga rata kiri dan rata kanan

--- main.go
+++ main.go
@@ -1,9 +1,75 @@
 package main
 
-import "fmt" 
+import "fmt"  
+
+type segitiga struct  {
+	length int
+	width int
+	heigh int
+} 
 
 func main()  {
 	const title string = "panjang"
+	
+	const panjang = 7
+	/*
+	fmt.Printf("* * * %s * * * \n", title) 
+	printH(panjang)
+	*/
+	fmt.Println()
+
+	fmt.Print("* * * segi tiga * * * \n")  
+
+	var bangunDatar interface{} = &segitiga{length: panjang}
+	n := bangunDatar.(*segitiga).length
+	rataKanan(n)
+	fmt.Println()
+	rataKiri(n) 
+}
+
+func rataKanan(n int)  {
+
+	/*
+				* 	[0,0] [0,1] [0,2] [0,3] *	4 = 5 - 1 
+			  * *   [1,0] [1,1] [1,2] *		*	3
+			* * *   [2,0] [2,1] *	  *		*	2
+		  * * * *   [3,0] *		*	  *		*	1
+		* * * * *	*	  *		*	  *		*	0
+	*/
+
+	for i := 1; i <= n; i++ {
+		x := n - i
+		for j := 0; j < n; j++ {
+			str := "*" 
+			if x > j {
+				str = " "
+			}
+			fmt.Print(str, " ")
+		}
+		fmt.Println()
+	}
+}
+
+func rataKiri(n int)  {
+
+	/*
+		* * * * *
+		* * * * 
+		* * * 
+		* * 
+		* 
+	*/
+
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			fmt.Print("*", " ")
+		}
+		fmt.Println()
+	}
+}
+
+func printH(length int)  {
+
 	/*
 		* mencetak gambar H, 
 		* panjang harus ganjil
@@ -16,14 +82,7 @@ func main()  {
 		*  =  =  =  *
 	
 	*/
-	const panjang = 7
 
-	fmt.Printf("* * * %s * * * \n", title) 
-	printH(panjang)
-
-}
-
-func printH(length int)  {
 	n := length
 
 	if n %2 == 0 {
@@ -37,9 +96,9 @@ func printH(length int)  {
 			strI = "*"
 		}
 		
-		for j := 0; j < n; j++ {
+		for j := 1; j <= n; j++ {
 			strJ := strI
-			if j == 0 || j == n-1{
+			if j == 1 || j == n {
 				strJ = "*"
 			}
 			fmt.Printf("%s %s", strJ, " ")
